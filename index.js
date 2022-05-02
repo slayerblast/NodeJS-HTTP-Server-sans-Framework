6602bff3b574f1a7a021969115e2ec9878f23644
Hery rakotoarisoa <hery_75@hotmail.fr>
2022-05-02 16:46:09 +0200

    Q5 provocation d'un erreur ligne 8 -> writee pour tester le renvoi du code error

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
+  try{
     if (req.url === "/") {
-        res.writeHead(200, {'content-type':'text/html'});
-        res.write('<h1>HELLO WORLD HERY</h1>');
-      }
+      res.writeHead(200, {'content-type':'text/html'});
+      res.writee('<h1>HELLO WORLD HERY</h1>');
+      
+    }
     else{
-      res.writeHead(404, {'content-type':'text/html'});
-      res.write('<h1>404 Page Introuvable</h1>');
-    }  
+    res.writeHead(404, {'content-type':'text/html'});
+    res.write('<h1>404 Page Introuvable</h1>');
+    }
+  } catch (err) {
+    res.writeHead(500, {'content-type':'text/html'});
+    res.write('<h1>500 Erreur Interne au Serveur</h1>');
+  }
+
+  
+      
     res.end()
 })
 
